04aec4d35099e094b216625b71ca914022776b1f
chenzhaoying <chenzhaoying6666@163.com>
2024-01-26 23:38:42 -0800

    first commit

--- server/api.js
+++ server/api.js
@@ -3,8 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const openaiApiKey = process.env.OPENAI_API_KEY;
+console.log(openaiApiKey);
 
 if (!openaiApiKey) {
+  
   console.error('OPENAI_API_KEY is not set.');
   process.exit(1);
 }
